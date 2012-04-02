6fcea7a90cd0227c25b361f8ea67c2fc785103b4
Michael Clark <michaelwclark@gmail.com>
2012-04-02 16:44:43 -0500

    Updated install.sh for new GTK Source View version as well as more verbose messages. Also cleaned up MKDIR commands to allow creation of parent directories

--- grails-gedit/install.sh
+++ grails-gedit/install.sh
@@ -3,8 +3,8 @@
 #Added directory structure verification
 MIME_PACKAGES="/usr/share/mime/packages/"
 GTKSOURCEVIEW="/usr/share/gtksourceview-2.0/language-specs/"
-TAGLIST="/usr/share/gedit-2/plugins/taglist/"
-BASH_COMPLETION="/etc/bash_completion.d"
+TAGLIST="/usr/share/gedit/plugins/taglist/"
+BASH_COMPLETION="/etc/bash_completion.d/"
 PLUGINS="$HOME/.gnome2/gedit/plugins/"
 SNIPPETS="$HOME/.gnome2/gedit/snippets/"
 STYLES="$HOME/.gnome2/gedit/styles/"
@@ -12,27 +12,32 @@ SCRIPTS="/usr/local/bin/"
 
 if [ ! -d $MIME_PACKAGES ]; then
 	echo "$MIME_PACKAGES doesn't exist. Creating..."
-	sudo mkdir $MIME_PACKAGES
+	sudo mkdir -p $MIME_PACKAGES
 fi
 
 if [ ! -d $GTKSOURCEVIEW ]; then
-	echo "$GTKSOURCEVIEW doesn't exist. Creating..."
-	sudo mkdir $GTKSOURCEVIEW
+	#Let's try version 3 first.
+	GTKSOURCEVIEW="/usr/share/gtksourceview-3.0/language-specs/"
+	
+	if [ ! -d $GTKSOURCEVIEW ]; then
+		echo "$MIME_PACKAGES doesn't exist. You may need to install missing requirments. Creating..."
+		sudo mkdir -p $GTKSOURCEVIEW
+	fi
 fi
 
 if [ ! -d $TAGLIST ]; then
 	echo "$TAGLIST doesn't exist. Creating..."
-	sudo mkdir $TAGLIST
+	sudo mkdir -p $TAGLIST
 fi
 
 if [ ! -d $PLUGINS ]; then
 	echo "$PLUGINS doesn't exist. Creating..."
-	sudo mkdir $PLUGINS
+	sudo mkdir -p $PLUGINS
 fi
 
 if [ ! -d $SNIPPETS ]; then
 	echo "$SNIPPETS doesn't exist. Creating..."
-	sudo mkdir $SNIPPETS
+	sudo mkdir -p $SNIPPETS
 fi
 
 #All directories should exist now, proceed with copy
@@ -49,3 +54,7 @@ sudo cp ./snippets/groovy.xml $SNIPPETS
 sudo cp ./snippets/gsp.xml $SNIPPETS
 
 sudo update-mime-database /usr/share/mime
+clear
+echo "Install succesfull!\r\n" 
+
+
